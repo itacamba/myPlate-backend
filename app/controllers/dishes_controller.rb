0d7d711187df3953ea8713947502c98155fd414c
itacamba <itacamba@live.com>
2020-06-14 18:01:59 -0400

    added dishes#show

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,6 +3,11 @@ class DishesController < ApplicationController
     def index
         render  json: Dish.all
     end
+
+    def show
+        dish = Dish.find(params[:id])
+        render json: dish
+    end
     
     def chef_dishes
         chef= User.find(params[:user_id])
