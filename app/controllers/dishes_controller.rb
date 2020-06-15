42df7b9e7ece257954ebc40cdb58378dfe185abe
itacamba <itacamba@live.com>
2020-06-14 21:25:50 -0400

    dishes#create working properly

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -9,11 +9,21 @@ class DishesController < ApplicationController
         render json: dish
     end
 
+    def create
+        dish = Dish.create(user_id: params[:user_id], name: params[:name], cuisine: params[:cuisine], description: params[:description], img: params[:img])
+        render json: dish
+    end
+
     def update
         dish = Dish.find(params[:id])
         dish.update(name: params[:name], cuisine: params[:cuisine], img: params[:img], description: params[:description])
         dish.save
         render json: dish
+    end
+    
+    def destroy
+        dish = Dish.find(params[:id])
+        dish.destroy
     end
     
     def chef_dishes
